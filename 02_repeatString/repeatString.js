32e8b594dc5223b40640cbb48ddf7e8b004e3e3f
htoohtetaung1 <htoohtetaung1@gmail.com>
2024-02-25 18:56:07 +0630

    Test and solve exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, times) {
+    let returnText = "";
+    if (times < 0) {
+        return "ERROR";
+    }
+        for (i = 0; i<times; i++) {
+            returnText += text;
+        }
+    return returnText;
 };
 
 // Do not edit below this line
